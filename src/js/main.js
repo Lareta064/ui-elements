6fd64c4fbd928ca3f83c17df08c418540bb605f4
Lareta <lareta064@gmail.com>
2021-02-06 21:24:38 +0300

    header-1

---
+++ src/js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+	/*===== HEADER GAMBURGER===========*/
+	const menuToggle = document.querySelector('#menu-toggle');
+	// const mobMenu = document.querySelector('.nav-menu');
+	const bodyEl = document.body;
+
+	if (menuToggle) {
+		menuToggle.addEventListener('click', function () {
+			if (this.classList.contains('active')) {
+				this.classList.remove('active');
+				// fixMenu.classList.remove('active');
+				bodyEl.classList.remove('noscroll');
+
+			} else {
+				this.classList.add('active');
+				// fixMenu.classList.add('active');
+				bodyEl.classList.add('noscroll');
+
+			}
+		});
+		window.addEventListener('resize', function () {
+			menuToggle.classList.remove('active');
+			bodyEl.classList.remove('noscroll');
+			// fixMenu.classList.remove('active');
+		});
+	}
+
+	/*====== Показать строку поиска в шапке======*/
+	
+	const searchBtn = document.getElementsByClassName('header-search-btn');
+
+	if(searchBtn){
+		for(let item of searchBtn){
+			item.addEventListener('click', function(){
+				const searhForm = item.closest('.search-form');
+				const searchInput = searhForm.querySelector('.header-search-input');
+				
+				if(searchInput.classList.contains('active')){
+					searchInput.classList.remove('active');
+				}else{
+					searchInput.classList.add('active');
+				}
+			});
+		}
+
+	}
+
+	/*====== Зафиксировать часть шапки по скроллу ======*/
+	const headerTop = document.querySelector('header.header');
+
+	window.addEventListener('scroll', function(){
+        if(this.innerWidth > 1199){
+            if(this.pageYOffset > 10){
+                headerTop.classList.add('header-slim');
+            }
+            else{
+                headerTop.classList.remove('header-slim');
+            }
+        }
+    });
+})
